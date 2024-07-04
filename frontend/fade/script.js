66dd181b9833c5d3559e79429354cdfc10fad93a
Daniella Setio <144111393+starryciell@users.noreply.github.com>
2024-07-04 09:16:54 +0800

    Add files via upload

---
+++ frontend/fade/script.js
@@ -0,0 +1,24 @@
+const observerOptions = {
+    root: null,
+    threshold: 0,
+    rootMargin: '0px 0px -50px 0px'
+};
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('in-view');
+            observer.unobserve(entry.target);
+        }
+    });
+}, observerOptions);
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+const sections = Array.from(document.getElementsByClassName('section'));
+
+for (let section of sections) {
+    observer.observe(section);
+}
+
+});
